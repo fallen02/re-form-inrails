20e5de7e73843035ef02247440df17cdbab42d94
fallen02 <000.sb13@gmail.com>
2024-01-12 02:39:00 +0530

    OK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -12,7 +17,22 @@ class UsersController < ApplicationController
     end
   end
 
+
+  def update
+    @user = @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit , status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:email, :username, :password)
   end
