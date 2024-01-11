57377bf4d9e8d80dff09e3dc6a1f0f0e5e8feb8d
fallen02 <000.sb13@gmail.com>
2024-01-12 02:49:10 +0530

    OK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
+
+  before_action :find_user, only: [:edit, :update]
   def new
     @user = User.new
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
 
@@ -19,13 +21,15 @@ class UsersController < ApplicationController
 
 
   def update
-    @user = @user = User.find(params[:id])
+
     if @user.update(user_params)
       redirect_to new_user_path
     else
       render :edit , status: :unprocessable_entity
     end
   end
+
+  private
 
   private
 
